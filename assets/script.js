7866672d3de55ab8439d38a132ab8d624af6e3ec
Krimeas <krimeass@gmail.com>
2022-08-28 16:34:54 -0500

    JavaScript part two file complete

--- assets/script.js
+++ assets/script.js
@@ -3,20 +3,38 @@
 //var shapes = ["triangle", "square", "pentagon", "circle"];
 //var studentInfo = ["Lu", 54, true];
 var topics = ['HTML', 'CSS', 'Git', 'JavaScript'];
+var randomTopic = topics[Math.floor(Math.random() * topics.length)];
+
+
+function listTopics(){
+  for(var x = 0; x <topics.length; x++) {
+    console.log(topics[x]);
+   }
+  }
 
 
 //if statement
-/*if (topic === 'HTML') {
-  console.log("Let's study HTML!");
-} else if (topic === 'CSS') {
-  console.log("Let's study CSS!");
-} else if (topic === 'Git') {
-  console.log("Let's study Git!")
-} else if (topic === 'JavaScript') {
-  console.log("Let's study JavaScript");
-} else {
-  console.log('please try again!');
-}*/
+function selectTopic(){
+  if (randomTopic === 'HTML') {
+     console.log("Let's study HTML!");
+   } else if (randomTopic === 'CSS') {
+     console.log("Let's study CSS!");
+   } else if (randomTopic === 'Git') {
+     console.log("Let's study Git!")
+   } else if (randomTopic === 'JavaScript') {
+     console.log("Let's study JavaScript");
+   } else {
+     console.log("please try again!");
+   }
+  }
+
+
+console.log('Here are the topics we learned though Prework;');
+listTopics();
+
+console.log('Which topic should we study first?');
+selectTopic()
+
 
 /*
 //shapes single log
@@ -27,6 +45,3 @@ for(var x = 0; x < shapes.length; x++) {
 }*/
 
 
-for(var x = 0; x <topics.length; x++) {
-  console.log(topics[x]);
-}
